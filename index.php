26976df4763cc0dbeb443fa6958e125592942a57
tropot <mangustar123@gmail.com>
2022-10-26 17:15:15 +0300

    wroking with db

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
     </head>
     <body>
         <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "testing";
+            $conn = new mysqli($servername,$username,$password,$dbname);
+            
             if (isset($_POST['submit'])) {
                 $numar = 0;
                 $nr_random = random_int(1, 25);
@@ -14,6 +20,44 @@
                 $numar = (int)$_POST['nr'];
                 $suma = $numar + $nr_random;
                 echo "sume : ".$suma.'<br />';
+
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                  }
+                  $sql = "INSERT INTO numeros (input_number, random_number, number_result)
+                    VALUES ($numar, $nr_random, $suma)";
+                    if ($conn->query($sql) === TRUE) {
+                        echo "numbers are posted to the database";
+                      } else {
+                        echo "Error: " . $sql . "<br>" . $conn->error;
+                      }
+                      $sql = "SELECT * FROM numeros";
+                      if($result = mysqli_query($conn, $sql)){
+                          if(mysqli_num_rows($result) > 0){
+                              echo "<table>";
+                                  echo "<tr>";
+                                      echo "<th>nr introdus  |</th>";
+                                      echo "<th>nr random    |</th>";
+                                      echo "<th>suma acestora|</th>";
+                                  echo "</tr>";
+                              while($row = mysqli_fetch_array($result)){
+                                  echo "<tr>";
+                                      echo "<td>" . $row['input_number'] . "</td>";
+                                      echo "<td>" . $row['random_number'] . "</td>";
+                                      echo "<td>" . $row['number_result'] . "</td>";
+                                  echo "</tr>";
+                              }
+                              echo "</table>";
+                              // Free result set
+                              mysqli_free_result($result);
+                          } else{
+                              echo "No records matching your query were found.";
+                          }
+                      } else{
+                          echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+                      }
+                    
+
             }
             else{
                 echo "nica introdus";
